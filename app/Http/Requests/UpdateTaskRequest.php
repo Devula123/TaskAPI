681f50bfa742d187acd070a091f74c9598f9a3ce
P_Pstrong <devxd8231@gmail.com>
2026-03-18 19:35:07 +0100

    Dodanie walidacji requestów i zasobów API

---
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\TaskStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateTaskRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'title' => ['sometimes', 'required', 'string', 'max:255'],
+            'status' => [
+                'sometimes',
+                'required',
+                'string',
+                Rule::in(array_column(TaskStatus::cases(), 'value')),
+            ],
+        ];
+    }
+}
+
